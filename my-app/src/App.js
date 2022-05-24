5cdbe777825356409830f418fa086b7ec7d80cdf
Ularbekova <milanaularbekova1@gmail.com>
2022-05-24 19:17:40 +0600

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Header from './components/Header'
+import About from './components/About'
+import Projekts from './components/Projects'
+function App(){
+  return(
+    <div className='App'>
+      
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react' 
+// import logo from ''
+const About = () => {
+  return (
+  <header>
+    <a href=''> Home</a>
+    <a href=''> About KNAU </a>
+    <a href=''> Education</a>
+    <a href=''> Enrollee</a>
+    <a href=''>Postgraduate studies </a>
+    <a href=''> Cooperation</a>
+    <a href=''> Training division</a>
+    <a href=''> education</a>
+    <a href=''> education</a>
+  </header>
+  )
+}
+
+export default About
